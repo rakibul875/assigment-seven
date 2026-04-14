44b5d0227ab01255554ece00c6b27d5f1d643ab3
rakibul875 <rakibuislamashik78@gmail.com>
2026-04-14 16:25:40 +0600

    Add Banner

---
+++ src/components/HomePage/Banner.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="container mx-auto my-10 bg-base-200 py-10">
+      <div className="hero">
+        <div className="hero-content text-center">
+          <div className="w-full">
+            <h1 className="text-5xl font-bold">
+              Friends to keep close in your life
+            </h1>
+            <p className="py-6">
+              Your personal shelf of meaningful connections. Browse, tend, and 
+              nurture the <br /> relationships that matter most.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/homePages/HomePage.jsx
+++ src/pages/homePages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../../components/HomePage/Banner';
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <Banner/>
         </div>
     );
 };
